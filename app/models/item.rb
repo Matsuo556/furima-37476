382728a9042881750474e35651450cc10d3af635
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-02-24 08:24:14 +0900

    Active　Storageを導入
    ストロングパラメーターの記載は未実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :shipping_time
+  has_one_attached :image
 
   with_options presence: true do
     validates :title
