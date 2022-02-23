3f5286647b6149ec1bb3eb3a66e9b8373d570f6c
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-02-23 12:18:05 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,11 @@ class User < ApplicationRecord
     validates :nickname
     validates :last_name,        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' }
     validates :first_name,       format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' }
-    validates :last_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/, message: "全角(カタカナ)で入力して下さい" }
-    validates :first_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/, message: "全角(カタカナ)で入力して下さい" }
+    validates :last_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/, message: '全角(カタカナ)で入力して下さい' }
+    validates :first_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/, message: '全角(カタカナ)で入力して下さい' }
     validates :birthday
   end
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
-
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
