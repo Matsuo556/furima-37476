f4ee61f0bfd2efbfe6d8315b84fa2562e75f201a
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-02-24 15:32:49 +0900

    ログイン有無で出品ページへのアクセスを制限

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
     validates :shipping_fee_id,     numericality: { other_than: 1, message: "can't be blank"} 
     validates :prefecture_id,       numericality: { other_than: 1, message: "can't be blank"} 
     validates :shipping_time_id,    numericality: { other_than: 1, message: "can't be blank"} 
-    validates :price,               numericality: { only_integer: true, message:'is invalid. Input half-width number.', in: 300..9999999 } 
+    validates :price,               numericality: { only_integer: true, message:'is invalid. Input half-width number.', in: 300..9999999, message:'is out of setting range.' } 
     validates :user
     validates :image
   end
