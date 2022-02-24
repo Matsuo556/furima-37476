c23fb07d3fa8fab22cfa413d0d1f2b2d884d412e
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-02-24 20:45:41 +0900

    FactoryBots記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,6 @@ class Item < ApplicationRecord
     validates :prefecture_id,       numericality: { other_than: 1, message: "can't be blank"} 
     validates :shipping_time_id,    numericality: { other_than: 1, message: "can't be blank"} 
     validates :price,               numericality: { only_integer: true, message:'is invalid. Input half-width number.', in: 300..9999999, message:'is out of setting range.' } 
-    validates :user
     validates :image
   end
 
