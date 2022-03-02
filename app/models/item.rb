85a9372c3bf9a14b5c5fafbc786101637fe16e2d
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-03-02 15:18:35 +0900

    priceのエラーメッセージ重複修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,9 +18,11 @@ class Item < ApplicationRecord
     validates :prefecture_id,       numericality: { other_than: 1, message: "can't be blank" }
     validates :shipping_time_id,    numericality: { other_than: 1, message: "can't be blank" }
     validates :price,               numericality: { only_integer: true, message: 'is invalid. Input half-width number.' }
-    validates :price,
-              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
-                              message: 'is out of setting range.' }
     validates :image
   end
+
+  validates :price,
+              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
+                              message: 'is out of setting range.' }
+
 end
